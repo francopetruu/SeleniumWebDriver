e84deee4bc239abf90ddd2d2a26188cd6d67fecd
Franco Petruccelli <fpetruccelli@symplr.com>
2022-01-12 15:38:43 -0300

    adding testcase to verify handling webelements

--- UnitTestProject1/TestScript/WebElement/TestWebElement.cs
+++ UnitTestProject1/TestScript/WebElement/TestWebElement.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestProject1.ComponentHelper;
+using UnitTestProject1.Settings;
 
 namespace UnitTestProject1.TestScript.WebElement
 {
-    internal class TestWebElement
+    [TestClass]
+    public class TestWebElement
     {
+        [TestMethod]
+        public void GetElement()
+        {
+            NavigationHelper.NavigateToUrl(ObjectRepository.Config.GetWebsite());
+            try
+            {
+                ObjectRepository.Driver.FindElement(By.XPath("//input[@name= 'uid']"));
+            } 
+            catch (NoSuchElementException e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+            
+        }
     }
 }
