02ccc5cb3658201903fb49ffdb184a79b88070c7
Franco Petruccelli <fpetruccelli@symplr.com>
2022-01-13 11:30:37 -0300

    adding checkbox helper class

---
+++ UnitTestProject1/TestScript/CheckBox/TestCheckBox.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestProject1.ComponentHelper;
+using UnitTestProject1.Settings;
+
+namespace UnitTestProject1.TestScript.CheckBox
+{
+    [TestClass]
+    public class TestCheckBox
+    {
+        [TestMethod]
+        public void CheckBoxTest()
+        {
+            NavigationHelper.NavigateToUrl(ObjectRepository.Config.GetWebsite());
+            CheckBoxHelper.IsCheckBoxChecked(By.XPath(""));
+        }
+    }
+}
