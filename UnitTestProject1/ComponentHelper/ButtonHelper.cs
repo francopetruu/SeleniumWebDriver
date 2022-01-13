827bd41aa478b1912ce67b75fd90bb6b71f862fb
Franco Petruccelli <fpetruccelli@symplr.com>
2022-01-13 12:08:10 -0300

    adding button helper class and button test class

---
+++ UnitTestProject1/ComponentHelper/ButtonHelper.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1.ComponentHelper
+{
+    internal class ButtonHelper
+    {
+        private static IWebElement element;
+        public static void ClickButton(By locator)
+        {
+            element = GenericHelper.GetElement(locator);
+            element.Click();
+        }
+    }
+}
---
+++ UnitTestProject1/TestScript/Button/TestButton.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestProject1.ComponentHelper;
+using UnitTestProject1.Settings;
+
+namespace UnitTestProject1.TestScript.Button
+{
+    [TestClass]
+    public class TestButton
+    {
+        [TestMethod]
+        public void ButtonTest()
+        {
+            NavigationHelper.NavigateToUrl(ObjectRepository.Config.GetWebsite());
+            TextBoxHelper.TypeOnTextBox(By.XPath("//input[@name= 'uid']"), ObjectRepository.Config.GetUserName());
+            TextBoxHelper.TypeOnTextBox(By.XPath("//input[@name= 'password']"), ObjectRepository.Config.GetPassword());
+            ButtonHelper.ClickButton(By.XPath("//input[@name= 'btnLogin']"));
+        }
+    }
+}
