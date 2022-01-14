a73e01bb0b4f1e504bee3656e88ea4a718ce3845
Franco Petruccelli <fpetruccelli@symplr.com>
2022-01-14 15:26:53 -0300

    screenshot test

---
+++ UnitTestProject1/TestScript/Screenshot/TakeScreenShot.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestProject1.ComponentHelper;
+using UnitTestProject1.Settings;
+
+namespace UnitTestProject1.TestScript.Screenshot
+{
+    [TestClass]
+    public class TakeScreenShot
+    {
+        [TestMethod]
+        public void TakeScreenShotTest()
+        {
+
+            NavigationHelper.NavigateToUrl(ObjectRepository.Config.GetWebsite());
+            TextBoxHelper.TypeOnTextBox(By.XPath("//input[@name= 'uid']"), ObjectRepository.Config.GetUserName());
+            TextBoxHelper.TypeOnTextBox(By.XPath("//input[@name= 'password']"), ObjectRepository.Config.GetPassword());
+            ButtonHelper.ClickButton(By.XPath("//input[@name= 'btnLogin']"));
+            ITakesScreenshot screenshot = ((ITakesScreenshot)ObjectRepository.Driver);
+            screenshot.GetScreenshot().SaveAsFile("screenshot.jpg", ScreenshotImageFormat.Jpeg);
+        }
+    }
+}
