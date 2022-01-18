ecdbf97945276bfd5e5468fa0503a9062ad70a37
Franco Petruccelli <fpetruccelli@symplr.com>
2022-01-18 16:20:08 -0300

    multiple browser windows test case

---
+++ UnitTestProject1/TestScript/MultipleBrowser/TestMultipleBrowserWindows.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestProject1.ComponentHelper;
+using UnitTestProject1.Settings;
+
+namespace UnitTestProject1.TestScript.MultipleBrowser
+{
+    [TestClass]
+    public class TestMultipleBrowserWindows
+    {
+        [TestMethod]
+        public void MultipleBrowserWindowsTest()
+        {
+            NavigationHelper.NavigateToUrl("https://www.w3schools.com/js/js_popup.asp");
+            ButtonHelper.ClickButton(By.XPath("(//a[text()= 'Try it Yourself »'])[1]"));
+            ReadOnlyCollection<string> windows = ObjectRepository.Driver.WindowHandles;
+            ObjectRepository.Driver.SwitchTo().Window(windows[1]);
+            ButtonHelper.ClickButton(By.XPath("//button[@id= 'runbtn']"));
+        }
+    }
+}
