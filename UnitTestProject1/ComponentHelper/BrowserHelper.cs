46bc216eed491ce50d3ebba603153544b34a8b39
Franco Petruccelli <fpetruccelli@symplr.com>
2022-01-18 10:47:16 -0300

    browser actions helper class

---
+++ UnitTestProject1/ComponentHelper/BrowserHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestProject1.Settings;
+
+namespace UnitTestProject1.ComponentHelper
+{
+    public class BrowserHelper
+    {
+        public static void BrowserMaximize()
+        {
+            ObjectRepository.Driver.Manage().Window.Maximize();
+        }
+        public static void BrowserBack()
+        {
+            ObjectRepository.Driver.Navigate().Back();
+        }
+        public static void BrowserForward()
+        {
+            ObjectRepository.Driver.Navigate ().Forward();
+        }
+        public static void BrowserRefresh()
+        {
+            ObjectRepository.Driver.Navigate().Refresh();
+        }
+    }
+}
