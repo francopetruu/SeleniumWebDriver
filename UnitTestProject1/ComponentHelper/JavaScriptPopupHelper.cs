a812b1e2cf6b8ea4e3688a60143344cfbffaf9c3
Franco Petruccelli <fpetruccelli@symplr.com>
2022-01-19 20:34:28 -0300

    popup helper class added

---
+++ UnitTestProject1/ComponentHelper/JavaScriptPopupHelper.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestProject1.Settings;
+
+namespace UnitTestProject1.ComponentHelper
+{
+    public class JavaScriptPopupHelper
+    {
+        public static bool IsAlertPresent()
+        {
+            try
+            {
+                ObjectRepository.Driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException e)
+            {
+                return false;
+            }
+        }
+        public static string GetPopupText()
+        {
+            if (IsAlertPresent())
+            {
+                return ObjectRepository.Driver.SwitchTo().Alert().Text;
+            }
+            else
+            {
+                return "";
+            }
+        }
+        public static void AcceptAlert()
+        {
+            if (IsAlertPresent())
+            {
+                ObjectRepository.Driver.SwitchTo().Alert().Accept();
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/TestScript/PopUps/TestPopups.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestProject1.ComponentHelper;
+using UnitTestProject1.Settings;
+
+namespace UnitTestProject1.TestScript.PopUps
+{
+    [TestClass]
+    public class TestPopups
+    {
+        [TestMethod]
+        public void PopupsTest()
+        {
+            NavigationHelper.NavigateToUrl("https://www.w3schools.com/js/js_popup.asp");
+            ButtonHelper.ClickButton(By.XPath("(//a[text()= 'Try it Yourself »'])[1]"));
+            BrowserHelper.SwitchToWindow(1);
+            IFrameHelper.SwitchToFrame(By.Id("iframeResult"));
+            ButtonHelper.ClickButton(By.XPath("//button[text()= 'Try it']"));
+            var alertText = JavaScriptPopupHelper.GetPopupText();
+            JavaScriptPopupHelper.AcceptAlert();
+            IFrameHelper.SwitchToParentFrame();
+        }
+    }
+}
