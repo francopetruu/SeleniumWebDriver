66d1fed5569add7a8fee4fe7b73ffe57be5698b4
Franco Petruccelli <fpetruccelli@symplr.com>
2022-01-20 01:26:58 -0300

    keyboard actions helper class and tests

---
+++ UnitTestProject1/ComponentHelper/KeyboardActionsHelper.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestProject1.Settings;
+
+namespace UnitTestProject1.ComponentHelper
+{
+    public class KeyboardActionsHelper
+    {
+        private static Actions actions;
+        public static void PressTwoKeys(string key, string letter)
+        {
+            actions = new Actions(ObjectRepository.Driver);
+            actions.KeyDown(key)
+                .SendKeys(letter)
+                .KeyUp(key)
+                .Build()
+                .Perform();
+        }
+        public static void PressThreeKeys(string firstkey, string secondKey, string letter)
+        {
+            actions = new Actions(ObjectRepository.Driver);
+            actions.KeyDown(firstkey)
+                .KeyDown(secondKey)
+                .SendKeys(letter)
+                .KeyUp(secondKey)
+                .KeyUp(firstkey)
+                .Build()
+                .Perform();
+        }
+    }
+}
