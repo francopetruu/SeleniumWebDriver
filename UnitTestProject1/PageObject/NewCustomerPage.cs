93608e21a4d83b9c6f9a0d666650981f9bc41680
Franco Petruccelli <fpetruccelli@symplr.com>
2022-01-24 19:17:07 -0300

    new customer page object creation and first action method

---
+++ UnitTestProject1/PageObject/NewCustomerPage.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1.PageObject
+{
+    public class NewCustomerPage : BasePage
+    {
+        private IWebDriver driver;
+        public NewCustomerPage(IWebDriver _driver) : base(_driver)
+        {
+            this.driver = _driver;
+        }
+
+        #region WebElements
+        #endregion
+
+        #region Actions
+        public void SetCustomerName(string name, string label)
+        {
+            GetInputElementByLabel(label).SendKeys(name);
+        }
+        #endregion
+
+        #region Navigation
+        #endregion
+    }
+}
